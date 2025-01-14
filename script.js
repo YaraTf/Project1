1518cac1881aa279f7a600e3c356118d8a7bb275
YaraTf <aroslavatimofeeva388@gmail.com>
2025-01-14 16:08:29 +0100

    100% finish

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const burgerIcon = header.querySelector(".burger-menu__icon");
 burger.addEventListener("click",function() {
   header.classList.toggle("header--mobile");
 
-
   if (header.classList.contains("header--mobile")) {
     burgerIcon.src = "images/burger-menu-close.svg";
   } else {
